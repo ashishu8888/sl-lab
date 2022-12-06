4794fc3f1b1ef4402ac06e8890b33f81bd16ea2e
Ashish <au2242524@gmail.com>
2022-12-06 18:40:27 +0530

    q3 added

---
+++ part A/q3/q3.py
@@ -0,0 +1,19 @@
+class rectangle:
+
+    def __init__(self,height,width):
+            self.width = width
+            self.height = height
+
+    def getArea(self):
+        return self.width*self.height
+
+
+
+w = int(input("Enter the width of rectangle\n"))
+h = int(input("Enter the height of rectangle\n"))
+
+rec1 = rectangle(w,h)
+
+print("The are of the rectangle is:\n",rec1.getArea())
+
+        
